0cd420535652d293569a637d3de54cc7fe1ed77e
Adi Vishnu Avula <95145136+adivishnu-a@users.noreply.github.com>
2023-02-09 23:31:46 +0530

    Delete Java directory

--- Java/CanWe.java
+++
@@ -1,5 +0,0 @@
-public class CanWe {
-    public static void main(String[] args) {
-        System.out.println("\"Can we print '\\' with System.out.println() statement?\"");
-    }
-}
--- Java/Names.java
+++
@@ -1,9 +0,0 @@
-public class Names {
-    public static void main(String args[]){
-        System.out.println("Vishnu\tvishnu@cse\tP1490\t9616969420");
-        System.out.println("Virat\tvirat@cse\tP1818\t8125981818");
-        System.out.println("Swetha\tswetha@cse\tP1410\t9753269173");
-        System.out.println("Amulya\tamulya@cse\tP5128\t7323610235");
-        System.out.println("Aditya\taditya@cse\tP1490\t9512604575");
-    }
-}
