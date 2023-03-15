3a35d226c6e1b66d37700a11f397ab0490d44760
Adi Vishnu Avula <adivishnu_avula@srmap.edu.in>
2023-03-15 12:03:27 +0530

    Create TryFinallyDemo.java

---
+++ Java/TryFinallyDemo.java
@@ -0,0 +1,17 @@
+class TryFinallyDemo
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			System.out.println("Hi");
+			//System.out.println(12/0);
+			System.out.println("Welcome");
+		}
+		finally
+		{
+			System.out.println("Hi SRM Students");
+		}
+		System.out.println("Java Prg");
+	}
+}
